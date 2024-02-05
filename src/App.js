addabf8a0defaa9df8f7b53730a718a63b0e827e
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 08:54:18 +0100

    test1
    modif1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React02
         </a>
       </header>
     </div>
