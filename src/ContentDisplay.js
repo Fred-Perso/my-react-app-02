8868740035ead677fd84383c785e37bf8c61592b
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 21:48:02 +0100

    Update ContentDisplay.js

--- src/ContentDisplay.js
+++ src/ContentDisplay.js
@@ -1,36 +1,23 @@
-// ContentDisplay.js
 import React, { useState, useEffect } from 'react';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import client from './contentfulConfig';
+import ReactHtmlParser from 'react-html-parser';
 
 const ContentDisplay = () => {
-  const [content, setContent] = useState({
-    fly: 'Valeur par défaut pour fly',
-    rich: 'Valeur par défaut pour rich',
-  });
+  const [content, setContent] = useState({ fly: null, rich: null });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchContent = async () => {
       try {
         const response = await client.getEntry('3jhKR5PJNniuvqy8882hYU');
-
-        if (response.fields) {
-          const newContent = {};
-
-          if (response.fields.fly) {
-            newContent.fly = response.fields.fly;
-          }
-
-          if (response.fields.rich) {
-            newContent.rich = documentToReactComponents(response.fields.rich);
-          }
-
-          setContent(newContent);
-        } else {
-          console.error('No fields found in Contentful response:', response);
-        }
+        setContent({
+          fly: response.fields.fly,
+          rich: response.fields.rich,
+        });
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching Contentful content:', error);
+        setLoading(false);
       }
     };
 
@@ -39,8 +26,14 @@ const ContentDisplay = () => {
 
   return (
     <div>
-      <p>Fly: {typeof content.fly === 'object' ? JSON.stringify(content.fly) : content.fly}</p>
-      <div>Rich: {content.rich}</div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p>Fly: {content.fly}</p>
+          <p>Rich Text: {content.rich && ReactHtmlParser(content.rich)}</p>
+        </div>
+      )}
     </div>
   );
 };
