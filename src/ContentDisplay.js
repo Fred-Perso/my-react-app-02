591485c34fdee32626d47da85c2b175c3d86b5de
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 15:00:02 +0100

    Update ContentDisplay.js

--- src/ContentDisplay.js
+++ src/ContentDisplay.js
@@ -1,1 +1,29 @@
+// ContentDisplay.js
+import React, { useState, useEffect } from 'react';
+import client from './contentfulConfig';
+
+const ContentDisplay = () => {
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const response = await client.getEntry('your_entry_id');
+        setContent(response.fields.helloWorld);
+      } catch (error) {
+        console.error('Error fetching Contentful content:', error);
+      }
+    };
+
+    fetchContent();
+  }, []);
+
+  return (
+    <div>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default ContentDisplay;
 
