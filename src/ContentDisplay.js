ff81c2e14cc267694acd4ddcc314724668e2132e
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 21:17:06 +0100

    Update ContentDisplay.js

--- src/ContentDisplay.js
+++ src/ContentDisplay.js
@@ -1,17 +1,26 @@
+// ContentDisplay.js
 import React, { useState, useEffect } from 'react';
 import client from './contentfulConfig';
 
 const ContentDisplay = () => {
-  const [content, setContent] = useState({ fly: null, rich: null });
+  const [content, setContent] = useState({
+    fly: 'Valeur par défaut pour fly',
+    rich: 'Valeur par défaut pour rich',
+  });
 
   useEffect(() => {
     const fetchContent = async () => {
       try {
-        const response = await client.getEntry('3jhKR5PJNniuvqy8882hYU');
-        setContent({
-          fly: response.fields['fly'], // Assurez-vous d'utiliser la locale correcte
-          rich: response.fields['rich'],
-        });
+        const response = await client.getEntry('3jhKR5PJNniuvqy8882hYU'); // Utilisez l'ID correct de votre entrée
+
+        // Ajoutez une condition pour éviter l'erreur
+        if (response.fields && response.fields.fly && response.fields.rich) {
+          // Utilisez un seul setContent avec un objet contenant tous les champs
+          setContent({
+            fly: response.fields.fly,
+            rich: response.fields.rich,
+          });
+        }
       } catch (error) {
         console.error('Error fetching Contentful content:', error);
       }
@@ -22,9 +31,9 @@ const ContentDisplay = () => {
 
   return (
     <div>
+      {/* Utilisez les valeurs de l'état dans votre composant */}
       <p>Fly: {content.fly}</p>
-  <p>Rich: {content.rich}</p>
-     
+      <p>Rich: {content.rich}</p>
     </div>
   );
 };
