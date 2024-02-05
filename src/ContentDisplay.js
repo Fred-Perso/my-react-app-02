e64f31840535cc85f813525bd50e6fb4e71c04f9
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 16:24:42 +0100

    Update ContentDisplay.js

--- src/ContentDisplay.js
+++ src/ContentDisplay.js
@@ -4,19 +4,21 @@ import client from './contentfulConfig';
 
 const ContentDisplay = () => {
   const [content, setContent] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchContent = async () => {
       try {
-        const response = await client.getEntry('titre');
+        const response = await client.getEntry('helloWorld');
         // Vérifiez si 'helloWorld' existe dans la réponse avant d'accéder à ses champs
-        if (response.fields && response.fields.titre) {
+        if (response.fields && response.fields.helloWorld) {
           setContent(response.fields.helloWorld);
         } else {
-          console.error('Error: "helloWorld" not found in Contentful response.');
+          setError('Error: "helloWorld" not found in Contentful response.');
         }
       } catch (error) {
         console.error('Error fetching Contentful content:', error);
+        setError('Error fetching Contentful content. See console for details.');
       }
     };
 
@@ -26,9 +28,9 @@ const ContentDisplay = () => {
   return (
     <div>
       {content && <p>{content}</p>} {/* Condition pour afficher le contenu uniquement s'il existe */}
+      {error && <p>{error}</p>} {/* Afficher l'erreur si elle existe */}
     </div>
   );
 };
 
 export default ContentDisplay;
-
