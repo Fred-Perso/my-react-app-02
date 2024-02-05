89d41c99def2f38d255b61f31416a6ae91d792a9
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 22:57:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   return (
    <div className="App">
       <header className="App-header">
-      <h1>My Contentful Ap6</h1>
       <ContentDisplay />
     </header>
     </div>
