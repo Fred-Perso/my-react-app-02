8acdbc8c77be00d2ddfaea761f9c905dfcceea21
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-05 20:57:14 +0100

    Update ContentDisplay.js

--- src/ContentDisplay.js
+++ src/ContentDisplay.js
@@ -1,16 +1,18 @@
-// ContentDisplay.js
 import React, { useState, useEffect } from 'react';
 import client from './contentfulConfig';
 
 const ContentDisplay = () => {
-  const [content, setContent] = useState(null);
+  const [content, setContent] = useState({ fly: null, rich: null });
 
   useEffect(() => {
     const fetchContent = async () => {
       try {
-        const response = await client.getEntry('3jhKR5PJNniuvqy8882hYU'); // Utilisez l'ID correct de votre entrée
-        setContent(response.fields['fly']); 
-        } catch (error) {
+        const response = await client.getEntry('3jhKR5PJNniuvqy8882hYU');
+        setContent({
+          fly: response.fields['fly'],
+          rich: response.fields['rich'],
+        });
+      } catch (error) {
         console.error('Error fetching Contentful content:', error);
       }
     };
@@ -20,7 +22,8 @@ const ContentDisplay = () => {
 
   return (
     <div>
-      <p>{content}</p>
+      <p>Fly: {content.fly}</p>
+      <p>Rich: {content.rich}</p>
     </div>
   );
 };
